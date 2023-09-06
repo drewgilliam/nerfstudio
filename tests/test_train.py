a9a2bc296adf753cc14acdf371d322e3daaf738e
Drew Gilliam <drew@visionsystemsinc.com>
2023-09-06 11:33:46 -0400

    test_train - set_timestamp, output_dir in pytest tmp_path

--- tests/test_train.py
+++ tests/test_train.py
@@ -9,8 +9,10 @@ from pathlib import Path
 import pytest
 
 from nerfstudio.configs.method_configs import method_configs
-from nerfstudio.data.dataparsers.blender_dataparser import BlenderDataParserConfig
-from nerfstudio.data.dataparsers.minimal_dataparser import MinimalDataParserConfig
+from nerfstudio.data.dataparsers.blender_dataparser import \
+    BlenderDataParserConfig
+from nerfstudio.data.dataparsers.minimal_dataparser import \
+    MinimalDataParserConfig
 from nerfstudio.engine.trainer import TrainerConfig
 from nerfstudio.models.vanilla_nerf import VanillaModelConfig
 from nerfstudio.scripts.train import train_loop
@@ -29,7 +31,7 @@ BLACKLIST = [
 ]
 
 
-def set_reduced_config(config: TrainerConfig):
+def set_reduced_config(config: TrainerConfig, tmp_path):
     """Reducing the config settings to speedup test"""
     config.machine.device_type = "cpu"
     if hasattr(config.pipeline.model, "implementation"):
@@ -56,11 +58,15 @@ def set_reduced_config(config: TrainerConfig):
     # remove viewer
     config.viewer.quit_on_train_completion = True
 
+    # timestamp & output directory
+    config.set_timestamp()
+    config.output_dir = tmp_path / "outputs"
+
     return config
 
 
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
-def test_train():
+def test_train(tmp_path):
     """test run train script works properly"""
     all_config_names = method_configs.keys()
     for config_name in all_config_names:
@@ -69,16 +75,16 @@ def test_train():
             continue
         print(f"testing run for: {config_name}")
         config = method_configs[config_name]
-        config = set_reduced_config(config)
+        config = set_reduced_config(config, tmp_path)
 
         train_loop(local_rank=0, world_size=0, config=config)
 
 
-def test_simple_io():
+def test_simple_io(tmp_path):
     """test to check minimal data IO works correctly"""
     config = method_configs["vanilla-nerf"]
     config.pipeline.datamanager.dataparser = MinimalDataParserConfig(data=Path("tests/data/minimal_parser"))
-    config = set_reduced_config(config)
+    config = set_reduced_config(config, tmp_path)
     train_loop(local_rank=0, world_size=0, config=config)
 
 
